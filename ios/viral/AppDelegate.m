c0d355c4eb947c6331b24e89453dfd27d793a892
Marlon Wiprud <marlon-wiprud@Marlons-MacBook-Air.local>
2019-03-05 14:18:07 -0500

    set up podfiles and configured RN-spotify and RN-firebase for ios

--- ios/viral/AppDelegate.m
+++ ios/viral/AppDelegate.m
@@ -6,6 +6,7 @@
  */
 
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -17,7 +18,7 @@
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-
+  [FIRApp configure];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"viral"
                                                initialProperties:nil
